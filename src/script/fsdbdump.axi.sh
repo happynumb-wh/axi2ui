f9d0ef0ed30b906632c6eab4c6e0af4ddcb88db8
zhuchenguang <zhuchenguang@mails.ucas.ac.cn>
2025-10-27 16:52:01 +0800

    fix(script): Fix file path in fsdbdump.axi.sh

--- src/script/fsdbdump.axi.sh
+++ src/script/fsdbdump.axi.sh
@@ -1,3 +1,4 @@
+# usage: in top directory: sh ./src/script/fsdbdump.axi.sh
 SIG_HEAD=osmc_axi_top_top/osmc_axi_top
 fsdbreport axi2ui.fsdb -exp "$SIG_HEAD/clock && $SIG_HEAD/io_ario_arvalid && $SIG_HEAD/io_ario_arready" -s "$SIG_HEAD/io_ario_arid" -csv -of h -o report.arid.txt
 fsdbreport axi2ui.fsdb -exp "$SIG_HEAD/clock && $SIG_HEAD/io_rio_rvalid && $SIG_HEAD/io_rio_rready && $SIG_HEAD/io_rio_rlast" -s "$SIG_HEAD/io_rio_rid" -csv -of h -o report.rid.txt
